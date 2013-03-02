8524c7355f97c6e254ca79dc28da6a7bcdecf73d
unknown <ugis.lazdins@gmail.com>
2013-03-02 10:38:39 +0200

    Forget to insert the images and style. Done now

---
+++ js/movesync/Dance.js
@@ -0,0 +1,17 @@
+function Dance()
+{
+	var frames;
+
+	var init = function(framesParam) {
+		frames = framesParam;
+	};
+	this.init = init;
+
+	var seekFameAt(time) {
+		//Seeks frame at specified time
+	}
+
+	var initFrameAtTime(time) {
+		//fires animation of a rame at time specified
+	}
+}
---
+++ js/movesync/DecriptionBar.js
@@ -0,0 +1,10 @@
+function DescriptionBar(){
+	var init = function(placeholder, descriptionJSON) {
+		//Inits the descriptions - generates description content into the HTML
+	}
+	this.init = init;
+
+	var highlightDescription = function(id) {
+		//Highlights the description that has the 'id' passed;
+	}
+}
