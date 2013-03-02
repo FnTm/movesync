4be91e017d294e189ec95250b757b8bcaf956230
Janis Peisenieks <janis@peisenieks.lv>
2013-03-02 11:16:15 +0300

    dirty YUI commit, just to enable easier hacking

--- app/scripts/movesync.js
+++ app/scripts/movesync.js
@@ -1,7 +1,5 @@
-$(document).ready(function(){
-	var canvas = oCanvas.create({ 
-		canvas: "#canvas", 
-		background: "#0cc", 
-		fps: 60
-	});
-});	
+var myAnim = new YAHOO.util.Anim('canvas', {
+    width: {
+        to: 400 
+    } 
+}, 1, YAHOO.util.Easing.easeOut);
