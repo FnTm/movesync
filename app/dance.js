84a74f68ed96567ce6f229638811b939d75c1d5d
unknown <ugis.lazdins@gmail.com>
2013-03-03 10:39:15 +0200

    Updates in sudmalinas

--- app/dance.js
+++ app/dance.js
@@ -151,7 +151,7 @@ var danceVar = {
 					{class:"girl", id: 4, movement : [{X: 250, Y: 625}], angle : 270}
 					]
 			},
-			{	//forming little windmills
+			{	//forming little windmills, moving 90 degrees
 				time : 20,
 				duration : 1000,
 				descriptionId : 2,
@@ -165,6 +165,22 @@ var danceVar = {
 					{class:"girl", id: 2, movement : [{X: 625, Y: 375}, {X: 625, Y: 500}], angle : 0},
 					{class:"girl", id: 3, movement : [{X: 125, Y: 375}, {X: 125, Y: 500}], angle : 0},
 					{class:"girl", id: 4, movement : [{X: 375, Y: 625}, {X: 375, Y: 500}], angle : 180}
+					]
+			},
+			{	//forming little windmills, moving 90 degrees
+				time : 21,
+				duration : 1000,
+				descriptionId : 2,
+				instructionId : 4,
+				path : [
+					{class:"boy", id: 1, movement : [{X: 625, Y: 375}, {X: 625, Y: 500}], angle : 0},
+					{class:"boy", id: 2, movement : [{X: 875, Y: 625}, {X: 875, Y: 500}], angle : 180},
+					{class:"boy", id: 3, movement : [{X: 375, Y: 625}, {X: 375, Y: 500}], angle : 180},
+					{class:"boy", id: 4, movement : [{X: 125, Y: 375}, {X: 125, Y: 500}], angle : 0},
+					{class:"girl", id: 1, movement : [{X: 875, Y: 375}, {X: 750, Y: 375}], angle : 90},
+					{class:"girl", id: 2, movement : [{X: 625, Y: 625}, {X: 750, Y: 625}], angle : 270},
+					{class:"girl", id: 3, movement : [{X: 125, Y: 625}, {X: 250, Y: 625}], angle : 270},
+					{class:"girl", id: 4, movement : [{X: 375, Y: 375}, {X: 250, Y: 375}], angle : 90}
 					]
 			},
 
